9359939756f0d7a4969bb6cf4ffc3b0b3a26449d
Diogo Camacho <diogocamacho@mordor.local>
2019-07-05 10:48:52 -0400

    removed 2 unnecessary functions

--- R/ext_ids.R
+++
@@ -1,7 +0,0 @@
-ext_ids <- function(drug_content) {
-  cas_id <- str_split(str_split(drug_content, "CAS: ")[[1]][2], "\n")[[1]][1]
-  pubchem_cid <- str_split(str_split(drug_content, "PubChem: ")[[1]][2], "\n")[[1]][1]
-  chebi_id <- str_split(str_split(drug_content, "ChEBI: ")[[1]][2], "\n")[[1]][1]
-  drugbank_id <- str_split(str_split(drug_content, "DrugBank: ")[[1]][2], "\n")[[1]][1]
-  return(tibble::tibble(cas_id = cas_id, pubchem_cid = pubchem_cid, chebi_id = chebi_id, drugbank_id = drugbank_id))
-}
