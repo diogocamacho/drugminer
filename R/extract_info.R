20bde320142be6400a02e3b0a9eca174aed7070b
Diogo Camacho <diogocamacho@mordor.local>
2019-07-05 09:38:45 -0400

    extract_info function to extract data on either drug or compound from the KEGG REST API POST call output

---
+++ R/extract_info.R
@@ -0,0 +1,21 @@
+#' Extract info
+#' 
+#' Given the output of a POST request to KEGG REST API, returns information on either drug or compound queried using the \link{\code{drug_info}} or \link{\code{compound_info}}, respectively. The input to the function comes from the \link{\code{kegg_query}} output.
+#' 
+#' @param kegg_content REST API content for a KEGG query on a drug or compound.
+#' @return Information on compound or drug, including chemical formula, external ids, disease indications, or pathways involved. Please refer to the \link{\code{drug_info}} or \link{\code(compound_info)} for details.
+#' 
+#' @example 
+#' q <- kegg_query(query_string = "aspirin")
+#' info <- extract_info(kegg_content = q)
+extract_info <- function(kegg_content) {
+  a1 <- str_split(kegg_content, "\n")[[1]][1] # <-- check if it's compound or drug
+  if (is.na(str_extract(str_split(a1, "\n")[[1]][1], "(D\\d{5})"))) {
+    message("Compound query. Extracting info.")
+    res <- compound_info(kegg_content)
+  } else {
+    message("Drug query. Extracting info.")
+    res <- drug_info(kegg_content)
+  }
+  return(res)
+}
